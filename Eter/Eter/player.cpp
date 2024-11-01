885fb94ad94d782da298fa01912cf72d333ad04b
VaidaRaluca <ralucavda@yahoo.com>
2024-11-01 20:24:28 +0200

    Am adaugat membrul cardsInhand de tip vector de carti clasei Player. Am implementat constructorul cu parametrii pentru clasa Player. Am implemenatat setteri si getteri pentru fiecare membru al clasei.

---
+++ Eter/Eter/player.cpp
@@ -0,0 +1,36 @@
+#include<cstdint>
+export module player;
+import card;
+import <vector>;
+import <string>;
+namespace eter {
+	export class Player
+	{
+	private:
+		std::string m_name;
+		uint32_t m_score;
+		std::string m_color;
+		std::vector<Card> m_cardsInHand;
+		bool m_hasUsedIllusion = false;
+	public:
+		Player() = default;
+		Player(const std::string& name, uint32_t score, const std::string& color, const std::vector<Card>& cards, bool hasUsedIllusion);
+		~Player() = default;
+
+		//Getteri
+		const std::string& GetName() const;
+		uint32_t GetScore() const;
+		const std::string& GetColor() const;
+		const std::vector<Card>& GetCardsInHand() const;
+		bool GetHasUsedIllusion() const;
+
+		//Setteri
+		void SetName(const std::string& name);
+		void SetScore(uint32_t score);
+		void SetColor(const std::string& color);
+		void SetCardsInHand(const std::vector<Card>& cards);
+
+		bool canPlayIllusion() const;
+		void useIllusion();
+	};
+}
