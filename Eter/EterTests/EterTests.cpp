014ee3a59235aa20a4933f01d5f3c71945445adf
VaidaRaluca <ralucavda@yahoo.com>
2024-12-22 13:58:15 +0200

    Am inceput implementarea unui dll cu scopul de unit tests. Deocamdata am facut doar setup-ul in speranta ca merge pentru toata lumea.

---
+++ Eter/EterTests/EterTests.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EterTests
+{
+	TEST_CLASS(EterTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
